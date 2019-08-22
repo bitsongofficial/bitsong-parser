c4de2f63c5870d8e8d3980a57ce4ce99e57e136a
Angelo <angelo@rcstart.it>
2019-08-22 02:28:46 +0200

    fix validator parser

--- src/common/ValidatorParser.ts
+++ src/common/ValidatorParser.ts
@@ -7,6 +7,7 @@ import { ValidatorRecord } from "../models/ValidatorRecordModel";
 import { IValidator } from "./CommonInterfaces";
 import { Bitsong } from "../services/Bitsong"
 import { getAddress } from 'tendermint/lib/pubkey';
+import * as BluebirdPromise from "bluebird";
 
 export class ValidatorParser {
     public pubkeyToBech32(pubkey, prefix = 'bitsongpub'): any {
@@ -37,74 +38,99 @@ export class ValidatorParser {
         return Promise.all([validatorList, validatorSet]);
     }
 
+
+
     parseBlock(block: number): Promise<any> {
         const bulkValidators = Validator.collection.initializeUnorderedBulkOp();
 
-        return this.getValidators(block).then(async ([validatorList, validatorSet]) => {
-            if (block > 1) {
-                for (var i in validatorList.validators) {
-                    let validator = validatorList.validators[i]
-    
-                    validator.voting_power = parseInt(validator.voting_power);
-                    validator.proposer_priority = parseInt(validator.proposer_priority)
+        const promises = [this.getValidators(block)]
 
-                    validator.address = getAddress(validator.pub_key)
-                    validator.accpub = await this.pubkeyToBech32(validator.pub_key, 'bitsongpub')
-                    validator.operator_pubkey = await this.pubkeyToBech32(validator.pub_key, 'bitsongvaloperpub')
-                    validator.consensus_pubkey = await this.pubkeyToBech32(validator.pub_key, 'bitsongvalconspub')
+        return Promise.all(promises).then(async (res) => {
+          const validatorList = res[0][0]
+          const validatorSet = res[0][1]
 
-                    let validatorData = validatorSet[0][validator.consensus_pubkey]
-                    if (validatorData) {
-                        if (validatorData.description.identity) {
-                            this.getValidatorProfileUrl(validatorData.description.identity).then(profile => {
-                                validator.profile_url = profile
-                            })
-                        }
-                        validator.operator_address = validatorData.operator_address;
-                        //validator.delegator_address = Meteor.call('getDelegator', validatorData.operator_address);
-                        validator.jailed = validatorData.jailed;
-                        validator.status = validatorData.status;
-                        validator.min_self_delegation = validatorData.min_self_delegation;
-                        validator.tokens = validatorData.tokens;
-                        validator.delegator_shares = validatorData.delegator_shares;
-                        validator.description = validatorData.description;
-                        validator.bond_height = validatorData.bond_height;
-                        validator.bond_intra_tx_counter = validatorData.bond_intra_tx_counter;
-                        validator.unbonding_height = validatorData.unbonding_height;
-                        validator.unbonding_time = validatorData.unbonding_time;
-                        validator.commission = validatorData.commission;
-                        validator.self_delegation = validator.delegator_shares;
+          if (block > 1) {
+              for (var i in validatorList.validators) {
+                let validator = validatorList.validators[i]
 
-                        bulkValidators.find({consensus_pubkey: validator.consensus_pubkey}).upsert().updateOne({$set:validator});
-                    }
+                validator.voting_power = parseInt(validator.voting_power);
+                validator.proposer_priority = parseInt(validator.proposer_priority)
+
+                validator.address = getAddress(validator.pub_key)
+                validator.accpub = this.pubkeyToBech32(validator.pub_key, 'bitsongpub')
+                validator.operator_pubkey = this.pubkeyToBech32(validator.pub_key, 'bitsongvaloperpub')
+                validator.consensus_pubkey = this.pubkeyToBech32(validator.pub_key, 'bitsongvalconspub')
+
+                let validatorData = validatorSet[0][validator.consensus_pubkey]
+                if (validatorData) {
+                    validator.operator_address = validatorData.operator_address;
+                    //validator.delegator_address = Meteor.call('getDelegator', validatorData.operator_address);
+                    validator.jailed = validatorData.jailed;
+                    validator.status = validatorData.status;
+                    validator.min_self_delegation = validatorData.min_self_delegation;
+                    validator.tokens = validatorData.tokens;
+                    validator.delegator_shares = validatorData.delegator_shares;
+                    validator.description = validatorData.description;
+                    validator.bond_height = validatorData.bond_height;
+                    validator.bond_intra_tx_counter = validatorData.bond_intra_tx_counter;
+                    validator.unbonding_height = validatorData.unbonding_height;
+                    validator.unbonding_time = validatorData.unbonding_time;
+                    validator.commission = validatorData.commission;
+                    validator.self_delegation = validator.delegator_shares;
+
+                    bulkValidators.find({consensus_pubkey: validator.consensus_pubkey}).upsert().updateOne({$set:validator});
                 }
+              }
+          }
+
+          if (bulkValidators.length === 0) return Promise.reject(`error in validators`)
+
+          return bulkValidators.execute().then((bulkResult: any) => {
+              winston.info("Processed block validators");
+
+              return Promise.resolve({block, validatorSet});
+          }).then(async (response: any) => {
+            const block = response.block
+            const validators = response.validatorSet[0]
+
+            // update profile pic each 720 blocks ~1h
+            if (block % 720 === 0) {
+              winston.info("Processing profile url validators");
+
+              for (var i in validators) {
+                const validator = validators[i]
+                if (validator.description.identity) {
+                  const profileurl = await this.getValidatorProfileUrl(validator.description.identity)
+                  validator.profile_url = profileurl
+                  bulkValidators.find({consensus_pubkey: validator.consensus_pubkey}).upsert().updateOne({$set:validator});
+                }
+              }
+
+              if (bulkValidators.length > 0) {
+                return bulkValidators.execute().then((bulkResult: any) => {
+                    winston.info("Processed profile urls validators");
+
+                    return Promise.resolve({block, validatorSet});
+                })
+              }
+
             }
-
-            if (bulkValidators.length === 0) return Promise.resolve();
-
-            return bulkValidators.execute().then((bulkResult: any) => {
-                winston.info("Processed block validators");
-    
-                return Promise.resolve();
-            });
-        })
+          })
+        }).catch(error => console.error(error))
     }
 
     public async parseValidators(blocks: any) {
         if (blocks.length === 0) return Promise.resolve();
 
-        blocks.flatMap(async (block: any) => {
-            const blockHeight = block.block.header.height
+        const promises = blocks.map((block, i) => {
+          const blockHeight = block.block.header.height
 
-            const promises = [this.parseBlock(blockHeight)]
+          return this.parseBlock(blockHeight)
+        })
 
-            Promise.all(promises).then(() => {
-                return 
-            })
-
-        }) 
-
-        return Promise.resolve(blocks);      
+        return Promise.all(promises).then((res: any) => {
+          return blocks
+        })
     }
 
     extractValidatorData(blockNumber: Number, validator: any) {

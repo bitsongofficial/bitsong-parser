ea739bc0179357b2d3d31d01a08fb142b51f7b48
Angelo <angelo@rcstart.it>
2019-08-23 17:00:25 +0200

    new validator parser logic

--- src/common/ValidatorParser.ts
+++ src/common/ValidatorParser.ts
@@ -1,144 +1,74 @@
 import * as winston from "winston";
-import * as bech32 from "bech32"
-import axios from 'axios';
+import * as bech32 from "bech32";
+import axios from "axios";
 import { Validator } from "../models/ValidatorModel";
-import { ValidatorSet } from "../models/ValidatorSetModel";
-import { ValidatorRecord } from "../models/ValidatorRecordModel";
 import { IValidator } from "./CommonInterfaces";
-import { Bitsong } from "../services/Bitsong"
-import { getAddress } from 'tendermint/lib/pubkey';
+import { Bitsong } from "../services/Bitsong";
+import { getAddress } from "tendermint/lib/pubkey";
 import * as BluebirdPromise from "bluebird";
 
 export class ValidatorParser {
-    public pubkeyToBech32(pubkey, prefix = 'bitsongpub'): any {
-        // '1624DE6420' is ed25519 pubkey prefix
-        let pubkeyAminoPrefix = Buffer.from('1624DE6420', 'hex')
-        let buffer = Buffer.alloc(37)
-        pubkeyAminoPrefix.copy(buffer, 0)
-        Buffer.from(pubkey.value, 'base64').copy(buffer, pubkeyAminoPrefix.length)
-        return bech32.encode(prefix, bech32.toWords(buffer))
-    }
+  public pubkeyToBech32(pubkey, prefix = "bitsongpub"): any {
+    // '1624DE6420' is ed25519 pubkey prefix
+    let pubkeyAminoPrefix = Buffer.from("1624DE6420", "hex");
+    let buffer = Buffer.alloc(37);
+    pubkeyAminoPrefix.copy(buffer, 0);
+    Buffer.from(pubkey.value, "base64").copy(buffer, pubkeyAminoPrefix.length);
+    return bech32.encode(prefix, bech32.toWords(buffer));
+  }
 
-    public getValidatorProfileUrl(identity: string): any {
-        if (identity.length == 16) {
-            return axios.get(`https://keybase.io/_/api/1.0/user/lookup.json?key_suffix=${identity}&fields=pictures`).then((response) => {
-                if (response.status === 200) {
-                    let them = response.data.them
-                    return them && them.length && them[0].pictures && them[0].pictures.primary && them[0].pictures.primary.url;
-                }
-            })
-        }
-
-        //return Promise.resolve()
-    }
-
-    getValidators(blockHeight): Promise<any[]> {
-        const validatorList = Bitsong.getValidators(blockHeight)
-        const validatorSet = Bitsong.getValidatorSet()
-        return Promise.all([validatorList, validatorSet]);
-    }
-
-
-
-    parseBlock(block: number): Promise<any> {
-        const bulkValidators = Validator.collection.initializeUnorderedBulkOp();
-
-        const promises = [this.getValidators(block)]
-
-        return Promise.all(promises).then(async (res) => {
-          const validatorList = res[0][0]
-          const validatorSet = res[0][1]
-
-          if (block > 1) {
-              for (var i in validatorList.validators) {
-                let validator = validatorList.validators[i]
-
-                validator.voting_power = parseInt(validator.voting_power);
-                validator.proposer_priority = parseInt(validator.proposer_priority)
-
-                validator.address = getAddress(validator.pub_key)
-                validator.accpub = this.pubkeyToBech32(validator.pub_key, 'bitsongpub')
-                validator.operator_pubkey = this.pubkeyToBech32(validator.pub_key, 'bitsongvaloperpub')
-                validator.consensus_pubkey = this.pubkeyToBech32(validator.pub_key, 'bitsongvalconspub')
-
-                let validatorData = validatorSet[0][validator.consensus_pubkey]
-                if (validatorData) {
-                    validator.operator_address = validatorData.operator_address;
-                    //validator.delegator_address = Meteor.call('getDelegator', validatorData.operator_address);
-                    validator.jailed = validatorData.jailed;
-                    validator.status = validatorData.status;
-                    validator.min_self_delegation = validatorData.min_self_delegation;
-                    validator.tokens = validatorData.tokens;
-                    validator.delegator_shares = validatorData.delegator_shares;
-                    validator.description = validatorData.description;
-                    validator.bond_height = validatorData.bond_height;
-                    validator.bond_intra_tx_counter = validatorData.bond_intra_tx_counter;
-                    validator.unbonding_height = validatorData.unbonding_height;
-                    validator.unbonding_time = validatorData.unbonding_time;
-                    validator.commission = validatorData.commission;
-                    validator.self_delegation = validator.delegator_shares;
-
-                    bulkValidators.find({consensus_pubkey: validator.consensus_pubkey}).upsert().updateOne({$set:validator});
-                }
-              }
+  public getValidatorProfileUrl(identity: string): Promise<any> {
+    if (identity.length == 16) {
+      return axios
+        .get(
+          `https://keybase.io/_/api/1.0/user/lookup.json?key_suffix=${identity}&fields=pictures`
+        )
+        .then(response => {
+          if (response.status === 200) {
+            let them = response.data.them;
+            return (
+              them &&
+              them.length &&
+              them[0].pictures &&
+              them[0].pictures.primary &&
+              them[0].pictures.primary.url
+            );
           }
-
-          if (bulkValidators.length === 0) return Promise.reject(`error in validators`)
-
-          return bulkValidators.execute().then((bulkResult: any) => {
-              winston.info("Processed block validators");
-
-              return Promise.resolve({block, validatorSet});
-          }).then(async (response: any) => {
-            const block = response.block
-            const validators = response.validatorSet[0]
-
-            // update profile pic each 720 blocks ~1h
-            if (block % 720 === 0) {
-              winston.info("Processing profile url validators");
-
-              for (var i in validators) {
-                const validator = validators[i]
-                if (validator.description.identity) {
-                  const profileurl = await this.getValidatorProfileUrl(validator.description.identity)
-                  validator.profile_url = profileurl
-                  bulkValidators.find({consensus_pubkey: validator.consensus_pubkey}).upsert().updateOne({$set:validator});
-                }
-              }
-
-              if (bulkValidators.length > 0) {
-                return bulkValidators.execute().then((bulkResult: any) => {
-                    winston.info("Processed profile urls validators");
-
-                    return Promise.resolve({block, validatorSet});
-                })
-              }
-
-            }
-          })
-        }).catch(error => console.error(error))
+        });
     }
+  }
 
-    public async parseValidators(blocks: any) {
-        if (blocks.length === 0) return Promise.resolve();
-
-        const promises = blocks.map((block, i) => {
-          const blockHeight = block.block.header.height
-
-          return this.parseBlock(blockHeight)
-        })
-
-        return Promise.all(promises).then((res: any) => {
-          return blocks
-        })
+  async parseBlock(block: number): Promise<any> {
+    try {
+      const bulkValidators = Validator.collection.initializeUnorderedBulkOp();
+      const validatorList = await Bitsong.getValidators(block);
+      const validatorSet = await Bitsong.getValidatorSet();
+      console.log(validatorSet);
+    } catch (err) {
+      throw err;
     }
+  }
 
-    extractValidatorData(blockNumber: Number, validator: any) {
-        return {
-            height: blockNumber,
-            address: validator.address,
-            exists: false,
-            voting_power: Number(parseInt(validator.voting_power))
-        };
-    }
+  public async parseValidators(blocks: any) {
+    if (blocks.length === 0) return Promise.resolve();
+
+    const promises = blocks.map((block, i) => {
+      const blockHeight = block.block.header.height;
+
+      return this.parseBlock(blockHeight);
+    });
+
+    return Promise.all(promises).then((res: any) => {
+      return blocks;
+    });
+  }
+
+  extractValidatorData(blockNumber: Number, validator: any) {
+    return {
+      height: blockNumber,
+      address: validator.address,
+      exists: false,
+      voting_power: Number(parseInt(validator.voting_power))
+    };
+  }
 }
--- src/services/Bitsong.ts
+++ src/services/Bitsong.ts
@@ -1,77 +1,74 @@
-import axios from 'axios';
+import axios from "axios";
+import fetch from "isomorphic-fetch";
 
 const config = require("config");
 
 export class Bitsong {
+  static getLastBlock(): Promise<any> {
+    return axios.get(config.get("RPC") + "/status").then(response => {
+      return response.data.result.sync_info.latest_block_height;
+    });
+  }
 
-    static getLastBlock(): Promise<any> {
-        return axios.get(config.get("RPC") + '/status')
-            .then((response) => {
-                return response.data.result.sync_info.latest_block_height
-            })
+  static async getValidators(blockHeight: Number): Promise<any> {
+    const validators = await axios
+      .get(`${config.get("RPC")}/validators?height=${blockHeight}`)
+      .then(res => JSON.parse(JSON.stringify(res.data.result)));
+
+    return validators;
+  }
+
+  static async getValidatorSet(): Promise<any> {
+    let validatorSet = [];
+
+    const validators = await axios
+      .get(`${config.get("LCD")}/staking/validators`)
+      .then(res => JSON.parse(JSON.stringify(res.data.result)));
+
+    const validatorsUnbonded = await axios
+      .get(`${config.get("LCD")}/staking/validators?status=unbonded`)
+      .then(res => JSON.parse(JSON.stringify(res.data.result)));
+
+    const validatorsUnbonding = await axios
+      .get(`${config.get("LCD")}/staking/validators?status=unbonding`)
+      .then(res => JSON.parse(JSON.stringify(res.data.result)));
+
+    for (var i in validators) {
+      validatorSet[validators[i].consensus_pubkey] = validators[i];
     }
 
-    static getValidators(blockHeight: Number): Promise<any> {
-        return axios.get(config.get("RPC") + '/validators?height=' + blockHeight)
-            .then((response) => {
-                return response.data.result
-            })
+    for (var i in validatorsUnbonded) {
+      validatorSet[validatorsUnbonded[i].consensus_pubkey] =
+        validatorsUnbonded[i];
     }
 
-    static async getValidatorSet(): Promise<any> {
-        let validatorSet = []
-
-        await axios.get(config.get("LCD") + '/staking/validators').then((response) => {
-            let validators = JSON.parse(JSON.stringify(response.data.result))
-
-            for (var i in validators) {
-                let validator = validators[i]
-
-                validatorSet[validator.consensus_pubkey] = validator
-            }
-        })
-
-        await axios.get(config.get("LCD") + '/staking/validators?status=unbonded').then((response) => {
-            let validators = JSON.parse(JSON.stringify(response.data.result))
-
-            for (var i in validators) {
-                let validator = validators[i]
-
-                validatorSet[validator.consensus_pubkey] = validator
-            }
-        })
-
-        await axios.get(config.get("LCD") + '/staking/validators?status=unbonded').then((response) => {
-            let validators = JSON.parse(JSON.stringify(response.data.result))
-
-            for (var i in validators) {
-                let validator = validators[i]
-
-                validatorSet[validator.consensus_pubkey] = validator
-            }
-        })
-
-        return Promise.all([validatorSet]);
+    for (var i in validatorsUnbonding) {
+      validatorSet[validatorsUnbonding[i].consensus_pubkey] =
+        validatorsUnbonded[i];
     }
 
-    static getBlock(blockId: Number): Promise<any> {
-        return axios.get(config.get("RPC") + '/block?height=' + blockId)
-            .then((response) => {
-                return response.data.result
-            })
-    }
+    return validatorSet;
+  }
 
-    static getTxsByBlock(blockId: Number): Promise<any> {
-        return axios.get(config.get("RPC") + '/tx_search?query="tx.height=' + blockId + '"')
-            .then((response) => {
-                return response.data.result.txs
-            })
-    }
+  static getBlock(blockId: Number): Promise<any> {
+    return axios
+      .get(config.get("RPC") + "/block?height=" + blockId)
+      .then(response => {
+        return response.data.result;
+      });
+  }
 
-    static getTxByHash(hash: String): Promise<any> {
-        return axios.get(config.get("LCD") + '/txs/' + hash)
-            .then((response) => {
-                return response.data
-            })
-    }
+  static getTxsByBlock(blockId: Number): Promise<any> {
+    return axios
+      .get(config.get("RPC") + '/tx_search?query="tx.height=' + blockId + '"')
+      .then(response => {
+        return response.data.result.txs;
+      });
+  }
+
+  static getTxByHash(hash: String): Promise<any> {
+    return axios.get(config.get("LCD") + "/txs/" + hash).then(response => {
+      return response.data;
+    });
+  }
 }

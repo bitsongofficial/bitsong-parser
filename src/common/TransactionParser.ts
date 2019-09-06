bc6d2b946cb72d5513dacdff7117bdbd7abe091e
Angelo <angelo@rcstart.it>
2019-09-07 01:49:45 +0200

    Feat: Parse MultiSig TX

--- src/common/TransactionParser.ts
+++ src/common/TransactionParser.ts
@@ -5,6 +5,7 @@ import { Account } from "../models/AccountModel";
 import { ITransaction } from "./CommonInterfaces";
 import { Sdk } from "../services/Sdk";
 import { sha256 } from "js-sha256";
+import { isArray } from "util";
 
 const config = require("config");
 
@@ -92,11 +93,30 @@ export class TransactionParser {
   }
 
   extractTransactionData(transaction: any) {
+    // TODO: add parser for MultiSig
+    // { pub_key:
+    //   { type: 'tendermint/PubKeyMultisigThreshold',
+    //     value: { threshold: '2', pubkeys: [Array] } },
+    //  signature:
+    //   'CgUIAxIBwBJAaRfaaBTAj7nWR+WAO599B2/NzowdRUeATh+c7tiKqotxDs+GB8+aYME9LY2zo3igIiTujHKkXFBtpZl0I4mpbxJAdU55ivyI1Yb4t+kzJ7vXVXGeiHsJhCFkZVh3nwcKpdMtvK6aMhH42j0BZfGS/rvUTHGgzckkVbYgNo1lugeBeA==' }
+
     const signatures = transaction.tx.value.signatures.map((signature: any) => {
-      return Sdk.pubkeyUserToBech32(
-        signature.pub_key.value,
-        config.get("bech32PrefixAccAddr")
-      );
+      // Multisig check
+      if (signature.pub_key.type === "tendermint/PubKeyMultisigThreshold") {
+        const keys = signature.pub_key.value.pubkeys.map((key: any) => {
+          return Sdk.pubkeyUserToBech32(
+            key.value,
+            config.get("bech32PrefixAccAddr")
+          );
+        });
+
+        return keys;
+      } else {
+        return Sdk.pubkeyUserToBech32(
+          signature.pub_key.value,
+          config.get("bech32PrefixAccAddr")
+        );
+      }
     });
 
     return {
@@ -104,7 +124,7 @@ export class TransactionParser {
       height: Number(transaction.height),
       status: transaction.logs ? Boolean(transaction.logs[0].success) : false,
       msgs: transaction.tx.value.msg,
-      signatures: signatures,
+      signatures: isArray(signatures[0]) ? signatures[0] : signatures,
       gas_wanted: Number(transaction.gas_wanted),
       gas_used: Number(transaction.gas_used),
       fee_amount: transaction.tx.value.fee.amount,
